811b6bb3287f118c32e7c23da6c7c29ed05a8499
Dalya Eidda <dalia.eidda@gmail.com>
2019-02-08 00:26:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name 
   
-  @@song_counter = 0 
+  @@song_count = 0 
   
   def initialize(name)
     @name = name
@@ -15,16 +15,16 @@ class Artist
   def add_song(song)
     @songs << song 
     song.artist = self
-    @@song_counter += 1
+    @@song_count += 1
   end 
   
   def add_song_by_name(name)
     name = Song.new(name)
-    @@song_counter += 1
+    @@song_count += 1
   end 
   
   def self.song_count 
-    @@song_counter 
+    @@song_count 
   end 
  
 end 
