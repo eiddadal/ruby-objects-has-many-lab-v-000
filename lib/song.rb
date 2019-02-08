f493c93c0bddbe54eeb874e7598b9ce8c4c84146
Dalya Eidda <dalia.eidda@gmail.com>
2019-02-08 00:24:57 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+attr_accessor :artist, :name 
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @@all << self  
+end 
+
+def self.all 
+  @@all
+end 
+
+def name 
+  @name 
+end 
+
+def artist 
+  @artist
+end 
+
+def artist_name 
+end 
+
+end 
